4bf53e48c1436eafb539d7594b75e96658fce3a1
Alex_Sukhov <Alekc-suhov@yandex.ru>
2024-02-26 18:37:09 +0300

    homework

---
+++ src/js/api/ChatAPI.js
@@ -0,0 +1,18 @@
+import createRequest from './createRequest';
+
+export default class ChatAPI {
+    constructor(protocol, url) {
+        this.url = url;
+        this.protocol = protocol;
+    }
+
+    create(data, callback) {
+        createRequest({
+            url: this.protocol + '://' + this.url,
+            sendMethod: 'POST',
+            method: 'new-user',
+            data,
+            callback,
+        });
+    }
+}
---
+++ src/js/api/createRequest.js
@@ -0,0 +1,26 @@
+const createRequest = async (options = {
+    url, sendMethod, method, id, data, callback,
+}) => {
+    const strRequest = `${options.url}/${options.method}`;
+
+    if (options.sendMethod === 'POST') {
+        fetch(strRequest, {
+            method: 'POST',
+            body: JSON.stringify(options.data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                options.callback(data);
+            })
+            .catch((error) => {
+                console.error(`Error: ${error}`);
+                options.callback(error);
+            });
+    }
+};
+
+export default createRequest;
